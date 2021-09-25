315edbaf13063478dbcd2a63e6b3e7743627bd5c
Tasyafira <tasya_syafira_29rpl@student.smktelkom-mlg.sch.id>
2021-09-25 11:17:13 +0700

    Add files via upload

---
+++ home_pelanggan.php
@@ -0,0 +1,11 @@
+<?php 
+    include "header_pelanggan.php";
+?>
+<center>
+    <div style="margin-top:50px;">
+<h2>Selamat Datang  <?=$_SESSION['nama']?> di Website Tokline.</h2>
+</div>
+</center>
+<?php
+    include "footer_pelanggan.php";
+?>
